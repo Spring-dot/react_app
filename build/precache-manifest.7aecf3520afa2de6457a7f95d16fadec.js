cc5fa4b0e73d7eb210479945b4e196b7dd140610
Spring-dot <akankshapriya2162000@gmail.com>
2020-04-17 11:36:42 +0530

    node_modules

---
+++ build/precache-manifest.7aecf3520afa2de6457a7f95d16fadec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4300387479e39c75e66a124376c083b5",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "f5eefa6f1dd22b0eb2ff",
+    "url": "/my-app/static/css/main.acf6e19c.chunk.css"
+  },
+  {
+    "revision": "38c58a404a3fb46dd5f4",
+    "url": "/my-app/static/js/2.b102e267.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.b102e267.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5eefa6f1dd22b0eb2ff",
+    "url": "/my-app/static/js/main.6163a3ea.chunk.js"
+  },
+  {
+    "revision": "7017a0a3783b2a9ce739",
+    "url": "/my-app/static/js/runtime-main.b51f6de3.js"
+  }
+]);
